7e4b5cd015dde0b79afa66bd57fd858c7e8e7f47
ma6174 <ma6174@163.com>
2013-03-17 15:23:29 +0800

    check dangerous code

--- protect.py
+++ protect.py
@@ -289,6 +289,65 @@ def judge_one_mem_time(solution_id,problem_id,data_num,time_limit,mem_limit,lang
     logging.debug(rst)
     return rst
 
+def check_dangerous_code(solution_id,language):
+    if language in ['python2','python3']:
+        code = file('/work/%s/main.py'%solution_id).readlines()
+        support_modules = [
+            're',       #正则表达式
+            'string',   #字符串处理
+            'scanf',    #格式化输入
+            'math',     #数学库
+            'cmath',    #复数数学库
+            'decimal',  #数学库，浮点数
+            'numbers',  #抽象基类
+            'fractions',#有理数
+            'random',   #随机数
+            'itertools',#迭代函数
+            'functools',#Higher order functions and operations on callable objects
+            'operator', #函数操作
+            'readline', #读文件
+            'json',     #解析json
+            'array',    #数组
+            'sets',     #集合
+            'queue',    #队列
+            'types',    #判断类型
+        ]
+        for line in code:
+            if line.find('import') >=0:
+                words = line.split()
+                tag = 0
+                for w in words:
+                    if w in support_modules:
+                        tag = 1
+                        break
+                if tag == 0:
+                    return False
+        return True
+    if language in ['gcc','g++']:
+        try:
+            code = file('/work/%s/main.c'%solution_id).read()
+        except:
+            code = file('/work/%s/main.cpp'%solution_id).read()
+        if code.find('system')>=0:
+            return False
+        return True
+    if language == 'java':
+        code = file('/work/%s/main.java'%solution_id).read()
+        if code.find('Runtime.')>=0:
+            return False
+        return True
+    if language == 'go':
+        code = file('/work/%s/main.go'%solution_id).read()
+        danger_package = [
+            'os',
+        ]
+        for item in danger_package:
+            if code.find('"%s"'%item) >= 0:
+                return False
+        return True
+
+
+
 def judge(solution_id,problem_id,data_count,time_limit,mem_limit,program_info,result_code,language):
     '''评测编译类型语言'''
     max_mem = 0
@@ -358,6 +417,9 @@ def run(problem_id,solution_id,language,data_count,user_id):
         "System Error":11,
         "Judging":12,
     }
+    if check_dangerous_code(solution_id,language) == False:
+        program_info['result'] = result_code["Runtime Error"]
+        return program_info
     compile_result = compile(solution_id,language)
     if compile_result is False:#编译错误
         program_info['result'] = result_code["Compile Error"]
